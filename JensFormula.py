468c38766b01861a0645fa6c8eb13bea86c9deb0
laaaars77 <lars.lu.mail@gmx.de>
2026-03-15 16:16:47 +0100

    small fix analytical formula, still not correct

--- JensFormula.py
+++ JensFormula.py
@@ -4,7 +4,7 @@ prob: int = 0
 
 for i in range(5):
     for j in range(5 - i):
-        throw1 = math.comb(5, 1) * (1/6)**i * (5/6)**(5-i)
+        throw1 = math.comb(5, i) * (1/6)**i * (5/6)**(5-i)
         throw2 = math.comb(5-i, j) * (1/6)**j * (5/6)**(5-i-j)
         throw3 = (1/6)**(5-i-j)
         prob += throw1 * throw2 * throw3
