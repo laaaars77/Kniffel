7df8ade100661476c1b45df1983644dba6dc2971
laaaars77 <lars.lu.mail@gmx.de>
2026-03-15 16:14:10 +0100

    Analytical formula (WIP)

---
+++ JensFormula.py
@@ -0,0 +1,13 @@
+import math
+
+prob: int = 0
+
+for i in range(5):
+    for j in range(5 - i):
+        throw1 = math.comb(5, 1) * (1/6)**i * (5/6)**(5-i)
+        throw2 = math.comb(5-i, j) * (1/6)**j * (5/6)**(5-i-j)
+        throw3 = (1/6)**(5-i-j)
+        prob += throw1 * throw2 * throw3
+
+print(f'{prob * 100:.4f}%')  # not correct yet, TODO
+
